79f2012180a50b3011649b07e01e389f13ba748c
deptcqldd2025 <deptc.qldd2025@gmail.com>
2026-06-09 02:12:12 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+/* SOL61 VERSION 5.1 HEPC */
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', (e) => {
+    // Để trống để Iframe tự do kéo dữ liệu từ Google
+});
